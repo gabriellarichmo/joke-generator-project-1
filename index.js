78d5ad37e5007713fb33ed28612277f07aa9a1be
Gabriella Richmond <gabriella.m.richmond@gmail.com>
2024-02-09 15:00:06 -0800

    form in html

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const url = 'https://official-joke-api.appspot.com/random_joke'
 
 //! GLOBAL USE STUFF
 
-const fetchJokes = () => {
-  return fetch(`${url}`)
-  .then(resp => resp.json())
-  .then(jokes => renderJokes(jokes))
-  .catch(err => console.log(err))
-}
+fetch(`${url}`)
+.then(resp => resp.json())
+.then(jokeData => {debugger})
