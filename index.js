1483ef032b7929f80a03014af8cc574b37ab8e45
Gabriella Richmond <gabriella.m.richmond@gmail.com>
2024-02-12 10:09:33 -0800

    html forms and css

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const url = 'https://official-joke-api.appspot.com/random_joke'
-
+const newJokeForm = document.querySelector('#new-joke')
 //! GLOBAL USE STUFF
 
-=======
 const fetchJokes = () => {
   return fetch(`${url}`)
   .then(resp => resp.json())
   .then(jokes => renderJokes(jokes))
   .catch(err => console.log(err))
 }
-//! Appending the <h1, h2> (Name and Type of joke) and <p> (Setup and punchline)
+//! Appending the <h3, h4> (Name and Type of joke) and <p> (Setup and punchline)
 //! To the parent li
+const selectType = document.querySelector(".joke-type-selection");
+const result = document.querySelector(".result");
+selectElement.addEventListener("change", (e) => {
+  result.textContent = `${e.target.value}`;
+});
 
+function getNewJoke() {
+  const newJokeSetup = document.querySelector('#new-joke-setup')
+  newJokeSetup.innerText = document.getElementById('new-joke-setup')
+  const newJokePunchline = document.querySelector('#new-joke-punchline')
+  newJokePunchline.innerText = document.getElementById('new-joke-punchline')
+  const newJokeType = document.querySelector('#new-joke-type')
+  newJokeType.innerText = document.getElementById('new-joke-type')
+  const newUserName = document.querySelector('#user-name')
+  newUserName.innerText = document.getElementById('user-name')
+  console.log(`Type: ${newJokeType.innerText}` + `Setup: ${newJokeSetup.innerText}` +
+  `Punchline: ${newJokePunchline.innerText}` + `Submitted By: ${newUserName.innerText}`)
+}
+//!Remidner to test function getNewJoke() inside the web console
 
 
 //const userJokeForm = 
@@ -22,4 +39,4 @@ const sumbitUserJoke = (e) => {
   e.preventDefault();
   const li = document.createElement('li')
     
-  }
+}
